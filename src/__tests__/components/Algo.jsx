9d5803475d71f6dc5a57fc1e2572a77d864b7c53
GalvisDeveloper <galvisdeveloper@gmail.com>
2023-04-25 11:31:42 -0500

    Test config

---
+++ src/__tests__/components/Algo.jsx
@@ -0,0 +1,9 @@
+import { describe, it, afterEach, beforeEach, expect } from "vitest";
+import { screen, cleanup, render, fireEvent } from "@testing-library/react";
+import Algo from "../../components/Algo";
+
+describe('group', () => {
+    it('should', () => {
+        render(<Algo />);
+    })
+})
---
+++ src/components/Algo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Algo = () => {
+  return (
+    <div>Algo</div>
+  )
+}
+
+export default Algo
